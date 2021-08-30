75158e419c961eb12efd4d8605eb4898fd039607
Carlos Interiano <carinthnd@gmail.com>
2021-08-29 20:56:31 -0600

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+// Creación de una interfaz
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  }
+
   agregar() {
-    console.log('Está es una prueba');
+    console.log( this.nuevo );
   }
 }
